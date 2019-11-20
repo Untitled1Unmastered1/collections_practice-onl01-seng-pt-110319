a9b1059aeaf46c2f431c6ffaec3d8d187a20a685
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-20 05:50:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,6 +42,13 @@ def sum_array(array)
 end 
 end 
 
-def add_s(array)
-  array[0,1,3]. each {|y,x,s| 
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
     
